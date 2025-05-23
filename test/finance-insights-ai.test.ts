363a40b4b7a32319067da2a886c083fcd4c2f011
Manoj Kumar <manojkumar.mr0707@gmail.com>
2025-05-23 20:37:40 +0530

    Initial commit

---
+++ test/finance-insights-ai.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinanceInsightsAi from '../lib/finance-insights-ai-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/finance-insights-ai-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinanceInsightsAi.FinanceInsightsAiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
